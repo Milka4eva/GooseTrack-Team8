678b4098c0f697a84813b2574842b3509c9f3bb0
Milka4eva <114588326+Milka4eva@users.noreply.github.com>
2023-09-04 20:11:55 +0000

    Deploying to gh-pages from @ Milka4eva/GooseTrack-Team8@11528ab0b9b2baebc0934d94ec566aeb039a0efd 🚀

--- static/js/995.a425225e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[995],{4995:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("p",{children:"Statistics"})})}}}]);
-//# sourceMappingURL=995.a425225e.chunk.js.map
