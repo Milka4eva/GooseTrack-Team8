7a3c1892781752ae271241606b7ad3253b1ece1a
Mila Petruk <mila.petruk@gmail.com>
2023-09-04 22:47:21 +0200

    updated authnav.jsx

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,13 +1,13 @@
 import { NavLink, NavLinkFocus, NavLinkHover } from './AuthNav.styled';
-import { NavLink } from 'react-router-dom';
+import { NavigationLink } from 'react-router-dom';
 
 export default function AuthNav({ formType }) {
   const fillRout = formType === 'register' ? '/login' : '/register';
   const fillContent = formType === 'register' ? 'Log In' : 'Sign up';
 
   return (
-    <NavLink to={fillRout}>
+    <NavigationLink to={fillRout}>
       {fillContent}
-    </NavLink>
+    </NavigationLink>
   );
 }
