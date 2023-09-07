c1045f6520dc189bbe4d10f627a65a03a68ca234
SerheyPopov <serheypopov1994@gmail.com>
2023-09-07 16:43:31 +0300

    update colum list

--- src/components/ChoosedDay/ChoosedDay.jsx
+++ src/components/ChoosedDay/ChoosedDay.jsx
@@ -1,15 +1,11 @@
 import { Wrap } from './ChoosedDay.styled';
 import { DayCalendarHead } from '../../components/DayCalendarHead/DayCalendarHead';
-// import { TasksColumnsList } from '../../components/TasksColumnsList/TasksColumnsList';
-// import { DayCalendarHead } from '../../components/DayCalendarHead/DayCalendarHead';
-import { TasksColumnsList } from '../../components/TasksColumnsList/TasksColumnsList';
+import  TasksColumnsList  from '../../components/TasksColumnsList/TasksColumnsList';
 
 export const ChoosedDay = () => {
   return (
     <Wrap>
       <DayCalendarHead />
-      {/* <TasksColumnsList categories={['to-do', 'in-progress', 'done']} /> */}
-      {/* <DayCalendarHead /> */}
       <TasksColumnsList />
     </Wrap>
   );
